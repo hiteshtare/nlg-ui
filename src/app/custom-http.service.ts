361c39b0c89b085c66012a06428c129963d6e9d8
Hitesh Tare <hiteshtare@gmail.com>
2019-03-04 11:57:23 +0530

    **API link Updated

--- src/app/custom-http.service.ts
+++ src/app/custom-http.service.ts
@@ -5,8 +5,9 @@ import { Platform } from 'ionic-angular';
 @Injectable()
 export class CustomHttpService {
 
-  public basepath = 'https://entry.dev15.na01.labs.omnipresence.io/api/nlg';
+  // public basepath = 'https://entry.dev15.na01.labs.omnipresence.io/api/nlg';
   // public basepath = 'http://192.0.4.185:8080';
+  public basepath = 'https://4167b522.ngrok.io/api/nlg';
 
   constructor(private _platform: Platform) {
     // if (this._platform.is("cordova")) {
